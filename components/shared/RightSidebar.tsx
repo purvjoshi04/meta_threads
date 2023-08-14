7d6e2952306bf7618ef7db33edaf28d9ff66d5a7
--replace-all <purvadjoshi@gmail.com>
2023-08-14 20:50:36 +0530

    Intial Commit

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,13 @@
 function RightSidebar() {
-    return<h1>
-    Rightsidebar</h1 >
+    return (
+        <section className="custom-scrollbar rightsidebar" >
+            <div className="flex flex-1 flex-justify-start">
+                <h3 className="text-heaing4-medium text-light-1">Suggested Communities</h3>
+            </div>
+            <div className="flex flex-1 flex-justify-start">
+                <h3 className="text-heaing4-medium text-light-1">Suggested Users</h3>
+            </div>
+        </section>
+    )
 }
 export default RightSidebar;
