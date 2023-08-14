2fc12d83048aa24941ab0a156dab3e3f36e207cf
--replace-all <purvadjoshi@gmail.com>
2023-08-14 18:58:13 +0530

    Intial Commit

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function LeftSidebar() {
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {/* <Link></Link> */}
+            </div>
+
+        </section>
+    )
+}
+export default LeftSidebar;
