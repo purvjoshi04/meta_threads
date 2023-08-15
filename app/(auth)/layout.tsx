e3a9b03d298759f3d2554f6316e551555ac8a412
--replace-all <purvadjoshi@gmail.com>
2023-08-15 13:15:01 +0530

    Intial Commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 import "../globals.css"
 
 export const metadata = {
-    title: 'Theads',
+    title: 'Threads',
     description: 'A Next.js 13 Meta Threads Application'
 }
 
