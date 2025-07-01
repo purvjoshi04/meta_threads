ffb0e8b2cbc7ca32039d7dc75fa2e3b535d1e6c3
Purv Joshi <purvadjoshi@gmail.com>
2025-07-01 23:19:57 +0530

    fix deployment issue

--- next.config.js
+++ next.config.js
@@ -1,35 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-ignoreBuildErrors: true,
+    ignoreBuildErrors: true,
   },
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-      typescript: {
-        ignoreBuildErrors: true,
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      
+    ],
+    domains: ['localhost:3000'],
+    typescript: {
+      ignoreBuildErrors: true,
     },
-  };
-  
-  module.exports = nextConfig;
+  },
+};
+
+module.exports = nextConfig;
